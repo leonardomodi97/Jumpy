3d43a1bc3602fbdd3343f23135afafebf4d1322c
leonardo <leonardomodi46@gmail.com>
2017-05-05 11:10:48 +0100

    first commit

---
+++ Jumpy/cmake-build-debug/CMakeFiles/Jumpy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Jumpy.dir/main.cpp.o"
+  "Jumpy.pdb"
+  "Jumpy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Jumpy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
